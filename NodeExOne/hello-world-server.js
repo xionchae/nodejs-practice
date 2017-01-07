3d19d1125fa2f3408c5e8170fb26d8261aa1e1df
xionmac <xionmac@192.168.0.13>
2017-01-07 17:42:45 +0900

    first Commit

--- NodeExOne/hello-world-server.js
+++ NodeExOne/hello-world-server.js
@@ -4,4 +4,3 @@ http.createServer(function handler(req, res) {
     res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
-console.log('Server running at http://127.0.0.1:1337/');
